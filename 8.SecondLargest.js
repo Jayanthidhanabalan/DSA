06c5e54a0936914d02466964c73a60e85fcb6424
Jayanthi <jayanthidhanabalan@gmail.com>
2025-06-25 13:28:04 +0530

    # Second Largest number

--- 8.SecondLargest.js
+++ 8.SecondLargest.js
@@ -1,1 +1,26 @@
 //Write a function to find the second largest number in an array
+
+function findSecondLargest(arr) {
+  if (arr.length < 2) {
+    return null;
+  }
+
+  let largest = -Infinity;
+  let secondLargest = -Infinity;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > largest) {
+      secondLargest = largest;
+      largest = arr[i];
+    } else if (arr[i] > secondLargest && arr[i] !== largest) {
+      secondLargest = arr[i];
+    }
+  }
+  return secondLargest === -Infinity
+    ? "No Second largest number"
+    : secondLargest;
+}
+
+let arr = [5, 5, 15];
+
+console.log(findSecondLargest(arr));
